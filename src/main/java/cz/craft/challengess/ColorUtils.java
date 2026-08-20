5e9107c20fcb71f29cc39ae082d41fa1ec44b0b3
tobik3332-plugins <faktnevim174@gmail.com>
2026-08-20 20:04:14 +0200

    Create ColorUtils.java

---
+++ src/main/java/cz/craft/challengess/ColorUtils.java
@@ -0,0 +1,21 @@
+package cz.craft.challengess;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+
+public class ColorUtils {
+
+    private static final MiniMessage MINI_MESSAGE = MiniMessage.miniMessage();
+    private static final LegacyComponentSerializer LEGACY = LegacyComponentSerializer.legacyAmpersand();
+
+    public static Component parse(String input) {
+        if (input == null || input.isEmpty()) {
+            return Component.empty();
+        }
+        if (input.contains("&")) {
+            return LEGACY.deserialize(input);
+        }
+        return MINI_MESSAGE.deserialize(input);
+    }
+}
